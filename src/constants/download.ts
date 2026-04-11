c01fd365fcf3824f634858cd3bf922c347d33e84
Ubuntu <ubuntu@ip-172-31-25-137.eu-north-1.compute.internal>
2026-04-11 10:38:15 +0000

    Revert "fix: update video format selector and correct YouTube cookies file path"
    This reverts commit f18df3511b03a634e6299f452a66d9a2cd74545d.

--- src/constants/download.ts
+++ src/constants/download.ts
@@ -7,8 +7,8 @@ export const DOWNLOAD = {
   AUDIO_FORMAT: "mp3",
   VIDEO_FORMAT: "mp4",
   VIDEO_FORMAT_SELECTOR:
-    "bestvideo[filesize<45M][ext=mp4]+bestaudio[ext=m4a]/bestvideo[height<=720][filesize<45M][ext=mp4]+bestaudio[ext=m4a]/bestvideo[height<=480][filesize<45M][ext=mp4]+bestaudio[ext=m4a]/bestvideo[height<=360][filesize<45M][ext=mp4]+bestaudio[ext=m4a]/best[filesize<45M][ext=mp4]/best[filesize<45M]",
+    "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best",
   INSTAGRAM_USER_AGENT: "Mozilla/5.0",
-  YOUTUBE_COOKIES_FILE: "/app/youtube_cookies.txt",
+  YOUTUBE_COOKIES_FILE: "./youtube_cookies.txt",
   INSTAGRAM_COOKIES_FILE: "./instagram_cookies.txt",
 } as const;
